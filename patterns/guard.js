d6ec8498e4719b4bc1afc2175e8fa218de6d737c
Sherin <sherin.daniel89@yahoo.com>
2025-08-16 20:26:23 -0400

    Vote and Concat

--- patterns/guard.js
+++ patterns/guard.js
@@ -18,7 +18,15 @@ export function multiply(num1, num2) {
  * @returns {string} "Who would you like to vote for?" if `age` is 18 or older
  */
 export function vote(age) {
-  // TODO
+  if (typeof age !== "number") {
+    return "Age not valid";
+  }
+  if (age < 18) {
+    return "You must be 18 or older to vote";
+  }
+  if (age >= 18) {
+    return "Who would you like to vot for?";
+  }
 }
 
 /**
@@ -28,5 +36,8 @@ export function vote(age) {
  * @returns `undefined` if either argument is not a string
  */
 export function concatenateStrings(str1, str2) {
-  // TODO
+  if (typeof str1 !== "strings" || typeof str2 !== "strings") {
+    return undefined;
+  }
+  return str1 + str2;
 }
